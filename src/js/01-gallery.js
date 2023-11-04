55218177ab64378d4f7810bd553838362266372f
ira089 <gvir2015@gmail.com>
2023-11-05 01:13:19 +0200

    js08_01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+const galleryUl = document.querySelector('.gallery');
+
+const markupImg = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"><a class="gallery__link"  href="${original}"> <img class="gallery__image" src="${original}"  alt="${description}" /></a></li>`
+  )
+  .join(' ');
+
+galleryUl.innerHTML = markupImg;
+console.log(markupImg);
+const settings = {
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+console.dir(settings);
+
+let gallery = new SimpleLightbox('.gallery__link', settings);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+console.dir(iframe);
+
+const player = new Player(iframe, {
+  id: 19231868,
+  width: 640,
+});
+// const data = { duration: 61.857, percent: 0.049, seconds: 3.034 };
+
+const onPlay = function (data) {
+  console.log('The currentTime ');
+  const timeData = data.seconds;
+  console.log(timeData);
+  //   _.throttle(localStorage.setItem('videoplayer-current-time', timeData), 1000);
+  localStorage.setItem('videoplayer-current-time', timeData);
+};
+
+player.on('timeupdate', onPlay);
+
+const timeGet = localStorage.getItem('videoplayer-current-time');
+console.log(timeGet);
+player
+  .setCurrentTime(timeGet)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
